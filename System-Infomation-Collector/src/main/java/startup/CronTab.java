aef4239cc40b6bbb50a05eb41a976ee3f50b1119
Coo1s <quangminhdaowork@gmail.com>
2024-06-20 14:59:19 +0700

    fix bug in About & crontab

--- System-Infomation-Collector/src/main/java/startup/CronTab.java
+++ System-Infomation-Collector/src/main/java/startup/CronTab.java
@@ -15,7 +15,7 @@ public class CronTab extends JPanel {
         setLayout(new BorderLayout());
 
         // Create the table model with column names
-        String[] columnNames = {"Schedule", "Command", "Comments"};
+        String[] columnNames = {"Schedule", "User", "Command"};
         tableModel = new DefaultTableModel(columnNames, 0);
         startupTable = new JTable(tableModel);
 
@@ -30,12 +30,12 @@ public class CronTab extends JPanel {
     private void fetchCrontabEntries() {
         try {
             // Execute the crontab -l command to list crontab entries
-            Process process = Runtime.getRuntime().exec("crontab -l");
+            Process process = Runtime.getRuntime().exec("cat /etc/crontab");
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line;
 
             while ((line = reader.readLine()) != null) {
-                if (line.trim().isEmpty()) {
+                if (line.trim().isEmpty() || line.startsWith("#")) {
                     continue;
                 }
                 
@@ -45,21 +45,17 @@ public class CronTab extends JPanel {
                 }
 
                 String schedule = String.join(" ", parts[0], parts[1], parts[2], parts[3], parts[4]);
+                
+                String user = parts[5];
 
                 StringBuilder commandBuilder = new StringBuilder();
-                for (int i = 5; i < parts.length; i++) {
+                for (int i = 6; i < parts.length; i++) {
                     commandBuilder.append(parts[i]).append(" ");
                 }
                 String command = commandBuilder.toString().trim();
 
-                String comments = "";
-                int commentIndex = command.indexOf('#');
-                if (commentIndex != -1) {
-                    comments = command.substring(commentIndex + 1).trim();
-                    command = command.substring(0, commentIndex).trim();
-                }
 
-                tableModel.addRow(new Object[]{schedule, command, comments});
+                tableModel.addRow(new Object[]{schedule, user, command});
             }
         } catch (Exception e) {
             e.printStackTrace();
